68cc0e229c597a40b0c16a5b0d698d99bf7bcf0c
mihailov83 <42243106+mihailov83@users.noreply.github.com>
2018-09-17 00:44:08 +0200

    Add files via upload
    Add files to zadanie8.5

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var a = 2,
+    h = 4,
+	triangleArea = a * h / 2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
