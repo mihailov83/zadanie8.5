ebc88b3401a8adec28f4859b9f29061f83ab6423
mihailov83 <42243106+mihailov83@users.noreply.github.com>
2018-09-24 22:55:12 +0200

    Delate alert line

--- scripts.js
+++ scripts.js
@@ -2,7 +2,5 @@
 
 var a = 2,
     h = 4,
-	triangleArea = a * h / 2;
-
-alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+    triangleArea=a*h/2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
